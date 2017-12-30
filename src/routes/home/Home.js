93feb734e57db9fef14afd0a9f20e103a0ecf023
Allan Watson <allan.watson91@gmail.com>
2017-12-30 21:49:27 +0000

    header

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,23 +1,16 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import classNames from 'classnames';
 import s from './Home.less';
+import Header from './header/Header';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className={s.home}>
         <div className={classNames(s.bg, s.right)} />
         <div className={classNames(s.bg, s.left)} />
+        <Header />
       </div>
     );
   }
